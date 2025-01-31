db2a58a84a025755660415a52530bad27a412152
avdv <advanderveer@gmail.com>
2025-01-31 19:36:16 +0100

    now with snapshotting for schema mage

--- stdmage/stdmageatlas/atlas.go
+++ stdmage/stdmageatlas/atlas.go
@@ -7,8 +7,12 @@ import (
 	"path/filepath"
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/advdv/stdgo/stdlo"
+	"github.com/failsafe-go/failsafe-go"
+	"github.com/failsafe-go/failsafe-go/retrypolicy"
+	"github.com/jackc/pgx/v5"
 	"github.com/magefile/mage/sh"
 )
 
@@ -22,11 +26,23 @@ var (
 // PatchFunc allows users of the mage targets to patch any migration that is generated.
 type PatchFunc = func(latestName, latestFilename string) error
 
+var (
+	// targeting the host and container for snapshotting.
+	snapshotTarget *pgx.ConnConfig
+	// name of the container that is restarted for the snapshot.
+	snapshotTargetContainer string
+	// directory where the snapshot is stored in.
+	snapshotDir = filepath.Join("migrations", "snapshot")
+)
+
 // Init inits the mage targets. The weird signature is to make Mage ignore this when importing.
 func Init(
 	atlasInspectEnv,
 	atlasLocalEnv,
 	atlasMigrationsDir string,
+	snapshotRelDir string,
+	snapshotTargetContainerName string,
+	snapshotTargetConnStr string,
 	hook PatchFunc,
 	_ ...[]string, // just here so Mage doesn't recognize a "init" target.
 ) {
@@ -34,6 +50,10 @@ func Init(
 	localEnv = atlasLocalEnv
 	migrationsDir = atlasMigrationsDir
 	patchHook = hook
+
+	snapshotTargetContainer = snapshotTargetContainerName
+	snapshotTarget, _ = pgx.ParseConfig(snapshotTargetConnStr)
+	snapshotDir = snapshotRelDir
 }
 
 // Inspect visualizes the schema.
@@ -55,6 +75,55 @@ func Diff(name string) error {
 	return sh.Run("atlas", "migrate", "diff", name,
 		"--env", localEnv,
 	)
+}
+
+// Apply applies the migrations for the provided Atlas environment.
+func Apply(env string) error {
+	return sh.Run("atlas", "migrate", "apply",
+		"--env", env)
+}
+
+// Snapshot dumps the migrated schema to an sql file using pg_dump.
+func Snapshot() error {
+	// restart the container postgres container, expecting it to be empheral
+	if err := sh.Run(`docker`, `compose`, `restart`, snapshotTargetContainer); err != nil {
+		return fmt.Errorf("failed to restart postgres to clean it: %w", err)
+	}
+
+	// wait until the newly started container is migrated
+	if _, err := failsafe.Get(func() (bool, error) {
+		if err := Apply(localEnv); err != nil {
+			return false, fmt.Errorf("failed to migrated database: %w", err)
+		}
+
+		return true, nil
+	}, retrypolicy.
+		Builder[bool]().
+		WithMaxRetries(10).
+		WithDelay(time.Millisecond*500).
+		WithMaxDuration(time.Second*20).
+		Build()); err != nil {
+		return fmt.Errorf("failed to retry: %w", err)
+	}
+
+	// run pg_dump to sql representation
+	stdlo.Must0(os.MkdirAll(snapshotDir, 0o744))
+	if err := sh.Run("docker", "run",
+		"--rm", "--network", "host",
+		"-v", filepath.Join(stdlo.Must1(os.Getwd()), snapshotDir)+":/snapshot",
+		"-e", "PGPASSWORD="+snapshotTarget.Password,
+		"postgres:17.2-alpine", "pg_dump",
+		"-h", snapshotTarget.Host,
+		"-p", fmt.Sprintf("%d", snapshotTarget.Port),
+		"-U", snapshotTarget.User,
+		"--schema-only",
+		"--no-comments",
+		"--no-owner",
+		"-b", "-v", "-f", "/snapshot/snapshot.sql", "postgres"); err != nil {
+		return fmt.Errorf("failed to run pg_dump: %w", err)
+	}
+
+	return nil
 }
 
 // Iterate replaces the latest migration file with the same name, for quicker development.
@@ -88,7 +157,8 @@ func Iterate(name string) error {
 		}
 	}
 
-	return nil
+	// finally, write a new snapshot after every iteration.
+	return Snapshot()
 }
 
 // getLatestMigrationName read the migrations directory for the latest migration defined.
