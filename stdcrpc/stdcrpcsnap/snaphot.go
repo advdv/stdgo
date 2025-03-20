ab59a80a0704b1f6e1d00f4582036cf44f6694ee
avdv <advanderveer@gmail.com>
2025-03-20 08:40:55 +0100

    make snapshot fails easier to debug

--- stdcrpc/stdcrpcsnap/snaphot.go
+++ stdcrpc/stdcrpcsnap/snaphot.go
@@ -2,7 +2,10 @@
 package stdcrpcsnap
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -10,6 +13,7 @@ import (
 	"testing"
 
 	"connectrpc.com/connect"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/tidwall/gjson"
 	"github.com/tidwall/sjson"
@@ -27,7 +31,7 @@ type Overwrite struct {
 }
 
 // OverwriteAssertFunc allows asserting overwitten values.
-type OverwriteAssertFunc func(tb testing.TB, v gjson.Result)
+type OverwriteAssertFunc func(tb assert.TestingT, v gjson.Result)
 
 // PinResponseValue is a helper that allows for dealing with response values that are not static so
 // cannot be predicted for the snapshot value. The optional 'assertActual' can still assert the actual
@@ -76,11 +80,17 @@ func ResponseSnapshotEq[O any](
 // SnapshotEq compares the protobuf message against a snapshot. If the snapshot file doesn't exist it
 // is created instead.
 func SnapshotEq(tb testing.TB, actMsg []byte, actOverwrites ...Overwrite) {
-	var err error
+	fmtActMsg, err := formatJSONData(actMsg)
+	require.NoError(tb, err)
+
 	for _, actOverwrite := range actOverwrites {
 		actVal := gjson.GetBytes(actMsg, actOverwrite.Path)
 		if actOverwrite.Assert != nil {
 			actOverwrite.Assert(tb, actVal)
+		}
+
+		if tb.Failed() {
+			tb.Logf("overwrite assert failed, actual JSON: %s", fmtActMsg)
 		}
 
 		actMsg, err = sjson.SetBytes(actMsg, actOverwrite.Path, actOverwrite.Value)
@@ -101,5 +111,14 @@ func SnapshotEq(tb testing.TB, actMsg []byte, actOverwrites ...Overwrite) {
 		require.NoError(tb, err)
 	}
 
-	require.JSONEqf(tb, string(expMsg), string(actMsg), "snapshot mismatch, actual JSON: %s", string(actMsg))
+	require.JSONEqf(tb, string(expMsg), string(actMsg), "snapshot mismatch, actual JSON: %s", fmtActMsg)
+}
+
+func formatJSONData(data []byte) (string, error) {
+	var dst bytes.Buffer
+	if err := json.Indent(&dst, data, "", " "); err != nil {
+		return "", fmt.Errorf("indent json data: %w", err)
+	}
+
+	return dst.String(), nil
 }
