126bd59cf000fb9e32c4c76965cf7c24a9adc94b
avdv <advanderveer@gmail.com>
2025-03-29 07:47:26 +0100

    pin asserts now include original value in error message

--- stdcrpc/stdcrpcsnap/pin_asserts.go
+++ stdcrpc/stdcrpcsnap/pin_asserts.go
@@ -12,21 +12,21 @@ import (
 func AssertRFC3339Nano() OverwriteAssertFunc {
 	return func(t assert.TestingT, v gjson.Result) {
 		exp, err := time.Parse(time.RFC3339Nano, v.Str)
-		assert.False(t, exp.IsZero())
-		assert.NoError(t, err)
+		assert.False(t, exp.IsZero(), "got: %v", v)
+		assert.NoError(t, err, "got: %v", v)
 	}
 }
 
 // AssertUUID is a re-usable check to assert overwritten values.
 func AssertUUID() OverwriteAssertFunc {
 	return func(t assert.TestingT, v gjson.Result) {
-		assert.NoError(t, uuid.Validate(v.Str))
+		assert.NoError(t, uuid.Validate(v.Str), "got: %v", v)
 	}
 }
 
 // AssertStringOfLength is a re-usable check to assert just length values.
 func AssertStringOfLength(n int) OverwriteAssertFunc {
 	return func(t assert.TestingT, v gjson.Result) {
-		assert.Len(t, v.Str, n)
+		assert.Len(t, v.Str, n, "got: %v", v)
 	}
 }
